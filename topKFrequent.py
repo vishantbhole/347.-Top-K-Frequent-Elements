56a835c803045c70f7b8dd1ea5ed1b7d106b523d
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-15 21:00:41 -0500

    Update topKFrequent.py

--- topKFrequent.py
+++ topKFrequent.py
@@ -15,3 +15,11 @@ class Solution(object):
         for n,c in count.items():
             freq[c].append(n)
 
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
