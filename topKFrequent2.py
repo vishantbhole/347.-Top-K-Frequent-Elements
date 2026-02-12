1f6b5946a2339d892080d7b841eac89161e652f7
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-02-11 20:13:11 -0600

    Add initial logic for topKFrequent2 method

--- topKFrequent2.py
+++ topKFrequent2.py
@@ -7,4 +7,7 @@ class Solution(object):
       def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
             counter = Counter(nums)
             heap = []
+            for key, val in counter.items():
+                  if len(heap) < k:
+                      heapq.heappush(heap, (val, key))
 
