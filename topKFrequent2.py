a165150a5cffe37a0d180e44833ea1a3f7b644f7
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-02-11 20:13:47 -0600

    Optimize heap operations in topKFrequent2
    Refactor heap handling to use pushpop for efficiency.

--- topKFrequent2.py
+++ topKFrequent2.py
@@ -10,4 +10,8 @@ class Solution(object):
             for key, val in counter.items():
                   if len(heap) < k:
                       heapq.heappush(heap, (val, key))
+                  else:
+                      heapq.heappushpop(heap, (val, key))
+
+            return [h[1] for h in heap]
 
